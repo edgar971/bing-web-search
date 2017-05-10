d529d8ddc1889f1bb8d0e32fde1d636b17343a71
Edgar Pino <me@edgarpino.com>
2017-05-10 13:54:32 -0500

    Initial commit

---
+++ src/Client.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace EPino\BingSearch;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client implements ClientInterface {
+
+    /**
+     * @var GuzzleClient
+     */
+    protected $client;
+
+
+    public function __construct() {
+
+    }
+
+    public function web() {
+
+        // TODO: Implement web() method.
+
+    }
+
+    public function image() {
+
+        // TODO: Implement image() method.
+
+    }
+
+    public function search() {
+
+        // TODO: Implement search() method.
+
+    }
+
+    public function news() {
+
+        // TODO: Implement news() method.
+
+    }
+
+
+}
---
+++ src/ClientInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EPino\BingSearch;
+
+interface ClientInterface {
+
+    public function web();
+
+    public function image();
+
+    public function search();
+
+    public function news();
+
+
+}
